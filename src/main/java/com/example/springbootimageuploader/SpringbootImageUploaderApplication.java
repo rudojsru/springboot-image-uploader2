f37f29b28e3951b001f43b29ab46c90954a63742
rudojsru <rudojsru@gmail.com>
2021-11-13 10:25:05 +0100

    Initial commit

---
+++ src/main/java/com/example/springbootimageuploader/SpringbootImageUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootimageuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootImageUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootImageUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootimageuploader/SpringbootImageUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootimageuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootImageUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
